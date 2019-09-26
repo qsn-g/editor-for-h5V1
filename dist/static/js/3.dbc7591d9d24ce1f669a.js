9214fd34c89196ca4454e9f2d165b23629c9c97a
阿星 <jiangzengxing@xiaomi.com>
2019-09-26 17:52:09 +0800

    delete old file

--- dist/static/js/3.dbc7591d9d24ce1f669a.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([3],{EJIF:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r={render:function(){var e=this.$createElement;return(this._self._c||e)("div")},staticRenderFns:[]};var a=n("VU/8")({},r,!1,function(e){n("UTv5")},"data-v-fe6a3f7c",null);t.default=a.exports},UTv5:function(e,t){}});
-//# sourceMappingURL=3.dbc7591d9d24ce1f669a.js.map
